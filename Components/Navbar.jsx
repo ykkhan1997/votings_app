f933d53a140d2caebae45808cd9dc52121995523
Yasir Saeed <86898726+ykkhan1997@users.noreply.github.com>
2023-08-17 19:55:43 +0300

    init commit

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
 
     const { currentAccount, connectWallet } = useContext(VotingContext);
     return (
-        <div className="flex relative backgroundColor w-full py-4  justify-between items-center">
+        <div className="flex absolute backgroundColor w-full py-4  justify-between items-center">
             <Link href={{pathname:"/"}}><Image src={Logo} alt="Logo" width={100} height={80} className="cursor-pointer"/></Link>
             {!currentAccount ? (
                 <button className="pinkBackground px-2 py-2 Color" onClick={connectWallet}>
