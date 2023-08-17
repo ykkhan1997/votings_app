215adfd49f7b87aa5215f8720935d7fc99eaa704
Yasir Saeed <86898726+ykkhan1997@users.noreply.github.com>
2023-08-17 21:58:33 +0300

    init commit

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,44 +1,36 @@
-import React, { useContext, useState } from 'react'
-import Image from 'next/image'
-import { Logo } from '@/assets';
-import { VotingContext } from '@/context/Voter';
+import React, { useContext, useState } from "react";
+import Image from "next/image";
+import { Logo } from "@/assets";
+import { VotingContext } from "@/context/Voter";
 import {AiFillLock,AiFillUnlock} from 'react-icons/ai';
-import Link from 'next/link';
+import Link from "next/link";
 const Navbar = () => {
-    const {currentAccount,connectWallet}=useContext(VotingContext);
-    const [openToogle,setOpenToogle]=useState(false);
+  const [toogle, setToogle] = useState(true);
+  const {currentAccount,connectWallet}=useContext(VotingContext);
   return (
-    <nav className='relative flex justify-between items-center w-full py-6 backgroundColor'>
-        <Image src={Logo} alt='Logo' width={100} height={60}/>
-        <div className='flex flex-1 justify-end items-center mr-4'>
+    <navbar className="flex justify-between items-center w-full py-6 backgroundColor">
+      <Image src={Logo} width={100} height={60} alt="logo"/>
+      <div className="flex flex-1 justify-end items-center mr-4">
         {
-            currentAccount?(<button className='h-12 px-2 py-2 bg-[#9a02ac] Color rounded font-medium tracking-wide inline-flex items-center space-x-2'>{openToogle?<AiFillUnlock onClick={()=>setOpenToogle(false)}/>:<AiFillLock onClick={()=>setOpenToogle(true)}/>}{currentAccount.slice(0,9)}...{currentAccount.slice(39)}</button>)
+            currentAccount?(<div className="bg-[#9a02ac] Color h-12  items-center  px-4 py-2 rounded font-medium tracking-wide inline-flex cursor-pointer">{toogle?<AiFillLock className="mr-2" onClick={()=>setToogle(false)}/>:<AiFillUnlock onClick={()=>setToogle(true)}  className="mr-2"/>}{currentAccount.slice(0,9)}...{currentAccount.slice(39)}</div>)
             :
-            (<button className='h-12 px-2 py-2 bg-[#9a02ac] Color rounded font-medium tracking-wide' onClick={()=>connectWallet()}>Connect Wallet</button>)
+            (<button className="bg-[#9a02ac] Color h-12  items-center  px-4 py-2 rounded font-medium tracking-wide inline-flex" onClick={()=>connectWallet()}>Connect Wallet</button>)
         }
+        <div
+          className={`${
+            !toogle ? "hidden" : "flex"
+          } p-6 absolute bg-discount_gradient top-20 right-0 mx-4 my-2 min-w-[140px] bg-[#9a02ac] z-10 rounded-xl sidebar`}
+        >
+          <ul className="list-none flex-col  justify-end items-center flex-1 space-y-2">
+            <li className="font-medium text-[16px] leading-5 Color"><Link href={{pathname:'/'}}>Home</Link></li>
+            <li className="font-medium text-[16px] leading-5 Color"><Link href={{pathname:'/candidate-registeration'}}>Candidate Reg</Link></li>
+            <li className="font-medium text-[16px] leading-5 Color"><Link href={{pathname:'/voters-registeration'}}>Voter Reg</Link></li>
+            <li className="font-medium text-[16px] leading-5 Color"><Link href={{pathname:'/voterList'}}>Voter List</Link></li>
+          </ul>
         </div>
-        {
-            openToogle && (
-                <div className='p-6 absolute  bg-[#9a02ac] top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl z-50'>
-                    <ul className='list-none flex-col justify-end items-center flex-1 space-y-4'>
-                        <li className='font-poppins font-medium text-[16px] cursor-pointer Color'>
-                            <Link href={{pathname:'/'}}>Home</Link>
-                        </li>
-                        <li className='font-poppins font-medium text-[16px] cursor-pointer Color'>
-                        <Link href={{pathname:'/candidate-registeration'}}>Candidate Registeration</Link>
-                        </li>
-                        <li className='font-poppins font-medium text-[16px] cursor-pointer Color'>
-                        <Link href={{pathname:'/voters-registeration'}}>Voters Registeration</Link>
-                        </li>
-                        <li className='font-poppins font-medium text-[16px] cursor-pointer Color'>
-                        <Link href={{pathname:'/voterList'}}>Voter List</Link>
-                        </li>
-                    </ul>
-                </div>
-            )
-        }
-    </nav>
-  )
-}
+      </div>
+    </navbar>
+  );
+};
 
-export default Navbar
+export default Navbar;
