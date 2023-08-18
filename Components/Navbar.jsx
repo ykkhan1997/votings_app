6663131ba51d7a223ab32d82e50eba9d0d489bed
Yasir Saeed <86898726+ykkhan1997@users.noreply.github.com>
2023-08-18 11:16:43 +0300

    some changes in navbar

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,70 +1,47 @@
-import React,{useState,useContext} from 'react'
-import Image from 'next/image';
-import Link from 'next/link';
-import {AiFillLock,AiFillUnlock} from 'react-icons/ai';
-import Style from '../styles/Navbar.module.css';
-//Internal Import
+import React, { useContext, useState } from 'react'
 import { VotingContext } from '@/context/Voter';
 import { Logo } from '@/assets';
+import Image from 'next/image';
+import  {AiFillLock,AiFillUnlock} from 'react-icons/ai';
+import Link from 'next/link';
 const Navbar = () => {
-  const {connectWallet,currentAccount}=useContext(VotingContext);
-  const [openNav,setOpenNav]=useState(true);
+  const [opeNav,setOpenNav]=useState(true);
+  const {currentAccount,connectWallet}=useContext(VotingContext);
   const openNavigation=()=>{
-    if(openNav){
-      setOpenNav(false);
+    if(!opeNav){
+      setOpenNav(true);
     }else{
-      if(!openNav){
-        setOpenNav(true);
+      if(opeNav){
+        setOpenNav(false);
       }
     }
   }
   return (
-    <div>
-    <div className={`w-[100%] height-[5.5rem] bg-[#231e39] mb-[2rem] backgroundColor`}>
-      <div className={` w-[80%] m-auto flex items-center justify-between text-white`}>
-        <div className={`cursor-pointer`}>
-          <Link href={{pathname:'/'}}>
-            <Image src={Logo} alt='logo' width={100} height={60}/>
-          </Link>
-        </div>
-        <div className={` bg-[#9a02ac] p-[.5rem] cursor-pointer rounded`}>
-          {
-            currentAccount?(
-              <div>
-                <div className={Style.connect_flex}>
-                  <button onClick={()=>openNavigation()}>
-                    {currentAccount.slice(0,9)}...{currentAccount.slice(39)}
-                  </button>
-                  {
-                    currentAccount && (
-                      <span className='cursor-pointer'>
-                        {openNav?(<AiFillUnlock onClick={()=>openNavigation()}/>):
-                        (<AiFillLock onClick={()=>openNavigation()}/>
-                        )
-                        }
-                        </span>
-                    )
-                  }
-                </div>
-              </div>
-            ):(
-              <button onClick={()=>connectWallet()}>Connect Wallet</button>
-            )
-          }
-        </div>
+    <div className='flex flex-col items-end   w-full'>
+      <div className='backgroundColor w-full px-2 py-2 flex justify-between items-center'>
+      <Image src={Logo} alt='Logo' width={100} height={80}/>
+      <div className='px-2 py-2 bg-[#9a02ac] rounded font-medium Color'>
+        {
+          currentAccount?(<div className='inline-flex items-center space-x-1 cursor-pointer'>
+            {opeNav?<AiFillLock onClick={()=>openNavigation()}/>:<AiFillUnlock onClick={()=>openNavigation()}/>}
+            {currentAccount.slice(0,9)}...{currentAccount.slice(39)}
+          </div>):(<button  onClick={()=>connectWallet()}>Connect Wallet</button>)
+        }
       </div>
-    </div>
-    {openNav && 
-      (
-        <div className={`flex flex-col justify-end absolute bg-[#9a02ac] p-6 w-[15rem] top-[4rem] rounded-sm z-10`}>
-          <p className='Color hover:text-white'><Link href={{pathname:'/'}}>Home</Link></p>
-          <p className='Color hover:text-white'><Link href={{pathname:'candidate-registeration'}}>Candidate Registeration</Link></p>
-          <p className='Color hover:text-white'><Link href={{pathname:'voters-registeration'}}>Voter Registeration</Link></p>
-          <p className='Color hover:text-white'><Link href={{pathname:'voterList'}}>Voter List</Link></p>
-        </div>
+      </div>
+     {
+      opeNav && (
+        <div className='top-20 flex justify-end  flex-1 min-w-[140px] p-6 rounded absolute bg-[#9a02ac] z-10'>
+        <ul className='Color font-medium space-y-2'>
+        <p><Link href={{pathname:'/'}}>Home</Link></p>
+        <p><Link href={{pathname:'candidate-registeration'}}>Candidate Registeration</Link></p>
+        <p><Link href={{pathname:'voters-registeration'}}>Voters Registeration</Link></p>
+        <p><Link href={{pathname:'voterList'}}>Voters List</Link></p>
+        </ul>
+      </div>
       )
-      }
-      </div>
+     }
+    </div>
   )
 }
 
